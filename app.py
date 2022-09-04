91f00808c85bd932ab23a89fb693aae279ad44e1
coolc4t <m.ikbenais@gmail.com>
2022-09-04 12:39:31 +0300

    Updating size of pictures

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, make_response
 import json
 import requests
+import os
 app = Flask(__name__)
 
-api_key = "flag{n0_st0nes_unturn3d}"
+api_key = os.getenv('API_KEY')
 
 @app.route('/', methods=['GET'])
 def main():
@@ -15,7 +16,6 @@ def get_a_cat_picture_link():
     cat_api_url = "https://api.thecatapi.com/v1/images/search"
     response = requests.get(cat_api_url, headers={"Content-Type":"application/json", "x-api-key":api_key})
     cat_response = json.loads(response.text)
-    print(cat_response)
     if len(cat_response)>0 and "url" in cat_response[0]:
         cat_url = cat_response[0]["url"]
         return cat_url
